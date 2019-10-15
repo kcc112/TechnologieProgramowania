67f26123b85c566911bbf064b6652bd369d1bff5
Kamil Celejewski <kamil12283@gmail.com>
2019-10-15 09:29:17 +0200

    [Add] Zdarzenie constructor

--- Zadanie1/Class2.cs
+++ Zadanie1/Class2.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Zadanie1
 {
-    class Class2
+    class Zdarzenie
     {
+        private Wykaz wykaz;
+        private OpisStanu opisStanu;
+        private DateTime dataWypozyczenia;
+        private DateTime dataZwrotu;
+
+        Zdarzenie(Wykaz wykaz, OpisStanu opisStanu, DateTime dataWypozyczenia, DateTime dataZwrotu)
+        {
+            this.wykaz = wykaz;
+            this.opisStanu = opisStanu;
+            this.dataWypozyczenia = dataWypozyczenia;
+            this.dataZwrotu = dataZwrotu;
+        }
     }
 }
