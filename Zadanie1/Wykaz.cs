9288b8047c4da476c825bb97a9ce8b33bc99f1b3
216727 <216727@edu.p.lodz.pl>
2019-10-16 19:33:17 +0200

    [Add] class Wykaz

---
+++ Zadanie1/Wykaz.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    class Wykaz
+    {
+        private String imie;
+        private String nazwisko;
+
+        Wykaz(String imie, String nazwisko)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+        }
+    }
+}
