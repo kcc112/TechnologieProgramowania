b0cb65e723a8f960038e818b41d3821ad98719cd
216727 <216727@edu.p.lodz.pl>
2019-10-22 21:57:16 +0200

    [Add] AddKatalog, GetKatalog, GetAll methods to DataRepository

--- Zadanie1/DataRepository.cs
+++ Zadanie1/DataRepository.cs
@@ -9,5 +9,20 @@ namespace Zadanie1
     public class DataRepository
     {
         private DataContext dataContext;
+
+        void AddKatalog(Katalog k)
+        {
+            dataContext.katalogi.Add(dataContext.katalogi.Count, k);
+        }
+
+        Katalog GetKatalog(int id)
+        {
+            return dataContext.katalogi[id];
+        }
+
+        Dictionary<int, Katalog> GetAll()
+        {
+            return dataContext.katalogi;
+        }
     }
 }
