c82f81014f6f46fbc34efdf668ad12e5764d41a1
Kamil Celejewski <kamil12283@gmail.com>
2019-10-28 19:54:18 +0100

    [Add] methods declaration in IDataRepository

--- Zadanie1/Zadanie1/IDataRepository.cs
+++ Zadanie1/Zadanie1/IDataRepository.cs
@@ -10,17 +10,28 @@ namespace Zadanie1
     {
         void AddKatalog(Katalog katalog, int id);
         void AddWykaz(Wykaz wykaz);
+        void AddOpisStanu(OpisStanu opisStanu);
+        void AddZdarzenie(Zdarzenie zdarzenie);
 
         Katalog GetKatalog(int id);
         Wykaz GetWykaz(int id);
+        OpisStanu GetOpisStanu(int id);
+        Zdarzenie GetZdarzenie(int id);
 
         IEnumerable<Katalog> GetAllKatalog();
         IEnumerable<Wykaz> GetAllWykaz();
+        IEnumerable<OpisStanu> GetAllOpisStanu();
+        IEnumerable<Zdarzenie> GetAllZdarzenie();
 
         void UpdateKatalog(Katalog katalog, int id);
         void UpdateWykaz(Wykaz wykaz, int id);
+        void UpdateOpisStanu(OpisStanu opisStanu, int id);
+        void UpdateZdarzenie(Zdarzenie zdarzenie, int id);
+
 
         void DeleteKatalog(int id);
         void DeleteWykaz(Wykaz wykaz);
+        void DeleteOpisStanu(OpisStanu opisStanu);
+        void DeleteZdarzenie(Zdarzenie zdarzenie);
     }
 }
