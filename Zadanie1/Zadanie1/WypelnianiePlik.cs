508ef68cee1022382f0bccc9aec4c720d8c250de
216727 <216727@edu.p.lodz.pl>
2019-10-29 22:16:36 +0100

    [Add] WypelnianiePlik

---
+++ Zadanie1/Zadanie1/WypelnianiePlik.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    class WypelnianiePlik : IDataFill
+    {
+        public void Fill(DataContext context)
+        {
+            string line;
+            int count = 0;
+
+            System.IO.StreamReader katalogi = new System.IO.StreamReader(@"dane/katalogi.txt");
+            while ((line = katalogi.ReadLine()) != null)
+            {
+                string[] data = line.Split(';');
+                context.katalogi.Add(count, new Katalog(data[0], data[1], data[2], count));
+                count++;
+            }
+            katalogi.Close();
+
+            System.IO.StreamReader opisyStanu = new System.IO.StreamReader(@"dane/opisyStanu.txt");
+            while ((line = opisyStanu.ReadLine()) != null)
+            {
+                string[] data = line.Split(';');
+                context.opisyStanu.Add(new OpisStanu(context.katalogi[int.Parse(data[0])], double.Parse(data[1], System.Globalization.CultureInfo.InvariantCulture), int.Parse(data[2])));
+            }
+            opisyStanu.Close();
+
+            System.IO.StreamReader wykazy = new System.IO.StreamReader(@"dane/wykazy.txt");
+            while ((line = wykazy.ReadLine()) != null)
+            {
+                string[] data = line.Split(';');
+                context.wykazy.Add(new Wykaz(data[0], data[1]));
+            }
+            wykazy.Close();
+
+            System.IO.StreamReader zdarzeniaDodania = new System.IO.StreamReader(@"dane/zdarzeniaDodania.txt");
+            while ((line = zdarzeniaDodania.ReadLine()) != null)
+            {
+                string[] data = line.Split(';');
+                context.zdarzenia.Add(new ZdarzenieDodanie(context.wykazy[int.Parse(data[0])], context.opisyStanu[int.Parse(data[1])], new DateTime(int.Parse(data[2]), int.Parse(data[3]), int.Parse(data[4]))));
+            }
+            zdarzeniaDodania.Close();
+
+            System.IO.StreamReader zdarzeniaKupna = new System.IO.StreamReader(@"dane/zdarzeniaKupna.txt");
+            while ((line = zdarzeniaKupna.ReadLine()) != null)
+            {
+                string[] data = line.Split(';');
+                context.zdarzenia.Add(new ZdarzenieKupno(context.wykazy[int.Parse(data[0])], context.opisyStanu[int.Parse(data[1])], new DateTime(int.Parse(data[2]), int.Parse(data[3]), int.Parse(data[4]))));
+            }
+            zdarzeniaKupna.Close();
+        }
+    }
+}
