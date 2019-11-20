fcfae7138fe507c0edb6fff93d84d0183d6bbd40
Kamil Celejewski <kamil12283@gmail.com>
2019-11-20 18:52:44 +0100

    [Fix] polymorphism serialization

--- Zadanie1/Zadanie1/Serializer/JsonFormatterBase.cs
+++ Zadanie1/Zadanie1/Serializer/JsonFormatterBase.cs
@@ -8,7 +8,7 @@ namespace Zadanie1.Serializer
     {
         public static void Serialize(DataContext dataContext, Stream stream)
         {
-            string json = JsonConvert.SerializeObject(dataContext, Formatting.Indented);
+            string json = JsonConvert.SerializeObject(dataContext, Formatting.Indented, new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.Objects, TypeNameHandling = TypeNameHandling.Auto });
 
             byte[] content = Encoding.ASCII.GetBytes(json);
             stream.Write(content, 0, content.Length);
@@ -16,11 +16,10 @@ namespace Zadanie1.Serializer
 
         public static void Deserialize(ref DataContext dataContext, Stream stream)
         {
-            JsonConverter[] converters = { new DataContextConverter() };
             StreamReader reader = new StreamReader(stream);
 
             string fileContent = reader.ReadToEnd();
-            dataContext = JsonConvert.DeserializeObject<DataContext>(fileContent, new JsonSerializerSettings() { Converters = converters });
+            dataContext = JsonConvert.DeserializeObject<DataContext>(fileContent, new JsonSerializerSettings() { PreserveReferencesHandling = PreserveReferencesHandling.Objects, TypeNameHandling = TypeNameHandling.Auto });
         }
     }
 }
--- Zadanie2/Tests/RecursionTest.cs
+++ Zadanie2/Tests/RecursionTest.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.IO;
-using System.Text;
+﻿using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Newtonsoft.Json;
 using Tests.Recurrent_Model;
 
 namespace Tests
