29cf91af7c10f91fdbae866757097cd1bde75bd2
Kamil Celejewski <kamil12283@gmail.com>
2020-01-01 19:50:15 +0100

    [Add] service layer crud

---
+++ Zadanie4/ServiceLayer/ServiceLayer/IDataService.cs
@@ -0,0 +1,16 @@
+﻿using DataLayer.SqlConnection;
+using System.Collections.Generic;
+
+namespace ServiceLayer
+{
+    interface IDataService
+    {
+        List<ProductCategory> GetAllProductCategories();
+
+        void AddProductCategory(string name);
+
+        void UpdateProductCategory(string name, int id);
+
+        void DeleteProductCategory(int id);
+    }
+}
