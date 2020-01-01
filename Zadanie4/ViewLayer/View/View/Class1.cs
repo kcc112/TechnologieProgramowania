b16f0739392eef7551f9b949529c0a9f788f7f75
Kamil Celejewski <kamil12283@gmail.com>
2020-01-01 23:14:18 +0100

    [Add] Main view Model

--- Zadanie4/ViewLayer/View/View/Class1.cs
+++ Zadanie4/ViewLayer/View/View/Class1.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ViewData
+namespace View
 {
-    public class Class1
+    class Class1
     {
+        MainViewModel mainViewModle = new MainViewModel();
     }
 }
