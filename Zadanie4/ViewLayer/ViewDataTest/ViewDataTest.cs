4d5a1b6e60d21eaa2eae50ec6424c14f6d6107d6
216727 <216727@edu.p.lodz.pl>
2020-01-15 20:47:22 +0100

    [Add] AddCategoryTest

--- Zadanie4/ViewLayer/ViewDataTest/ViewDataTest.cs
+++ Zadanie4/ViewLayer/ViewDataTest/ViewDataTest.cs
@@ -1,38 +1,35 @@
 ﻿using System;
+using DataLayer;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ViewData.ViewModel;
+using System.Linq;
 
 namespace ViewDataTest
 {
     [TestClass]
     public class ViewDataTest
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void AddCategoryTest()
         {
             IViewModelHelper viewModelHelper = new ViewModelHelperFake();
             MainViewModel viewModel = new MainViewModel(viewModelHelper);
+            viewModel.AddCategoryCommand.Execute(null);
+            Assert.AreEqual(5, viewModel.DataLayer.GetAllProductCategories().ToList().Count);
+
         }
 
         [TestMethod]
-        public void AddCategory()
+        public void RemoveCategoryTest()
         {
-            IViewModelHelper viewModelHelper = new ViewModelHelperFake();
-            MainViewModel viewModel = new MainViewModel(viewModelHelper);
+            
         }
 
         [TestMethod]
-        public void RemoveCategory()
+        public void UpdateCategoryTest()
         {
-            IViewModelHelper viewModelHelper = new ViewModelHelperFake();
-            MainViewModel viewModel = new MainViewModel(viewModelHelper);
-        }
-
-        [TestMethod]
-        public void UpdateCategory()
-        {
-            IViewModelHelper viewModelHelper = new ViewModelHelperFake();
-            MainViewModel viewModel = new MainViewModel(viewModelHelper);
+            
         }
     }
 }
