9f119f846c17b3266b4237b244ce92d7cfeb41f1
216727 <216727@edu.p.lodz.pl>
2020-01-15 18:49:42 +0100

    [Fix] Add button column

--- Zadanie4/ViewLayer/ViewDataTest/ViewDataTest.cs
+++ Zadanie4/ViewLayer/ViewDataTest/ViewDataTest.cs
@@ -13,5 +13,26 @@ namespace ViewDataTest
             IViewModelHelper viewModelHelper = new ViewModelHelperFake();
             MainViewModel viewModel = new MainViewModel(viewModelHelper);
         }
+
+        [TestMethod]
+        public void AddCategory()
+        {
+            IViewModelHelper viewModelHelper = new ViewModelHelperFake();
+            MainViewModel viewModel = new MainViewModel(viewModelHelper);
+        }
+
+        [TestMethod]
+        public void RemoveCategory()
+        {
+            IViewModelHelper viewModelHelper = new ViewModelHelperFake();
+            MainViewModel viewModel = new MainViewModel(viewModelHelper);
+        }
+
+        [TestMethod]
+        public void UpdateCategory()
+        {
+            IViewModelHelper viewModelHelper = new ViewModelHelperFake();
+            MainViewModel viewModel = new MainViewModel(viewModelHelper);
+        }
     }
 }
